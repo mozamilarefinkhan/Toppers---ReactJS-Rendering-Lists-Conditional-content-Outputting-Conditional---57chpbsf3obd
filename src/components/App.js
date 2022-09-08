e91ea63a8e8e8f2bfdb71c213fb0c3e9152eac5d
Mozamil Arefin Khan <101820486+mozamilarefinkhan@users.noreply.github.com>
2022-09-08 23:51:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,86 +1,68 @@
-import React, {Component, useState} from "react";
 import '../styles/App.css';
+import React, { Component, useState } from "react";
 
 class App extends Component {
-	constructor(props)
-	{
-		super(props);
-		this.state = {
-			list : [
-				{
-					name : "naman",
-					percent : 60.0000
-				},
-				{
-					name : "ritesh",
-					percent : 99.9431
-				},
-				{
-					name : "hitesh",
-					percent : 65.3365
-				},
-				{
-					name : "shreya",
-					percent : 80.6400
-				},
-				{
-					name : "ankur",
-					percent : 78.8970
-				},
-				{
-					name : "vanshu",
-					percent : 78.9876
-				},
-				{
-					name : "krupal",
-					percent : 85.3214
-				},
-			],
-		}
-	};
-    render() {
-    	return(
-    		<div id="main">
-    			{
-    				//Correct the percentage conditional for correct output
-    				this.state.list.map(function(item,index){
-				   		if(item.percent > 75)
-				   		{
-				   			return(
-				   				//Conditional statement in the className of below div to give callName="bg-pink"
-				   				//if the percent >= 90 else className = "" 
-				   				<div key={index} className={"bg-pink"}>
-					   				<div className="name">
-										{item.name}
-					   				</div>
-					   				<div className="percent">
-										{item.percent}
-					   				</div>
-				   				</div>
-				   				);
-				   		}
-	                                        if(item.percent>=90)
-                                               {
-                                                 
-                                                        return(
-                                                            <div key={index} className={"pink-green"}>
-					   				<div className="name">
-					   					{item.name} 
-					   				</div>
-					   				<div className="percent">
-					   					{item.percent}
-					   				</div>
-				   				</div>
-                                                        )
-                                               }
-			       })
-			    }
-    		</div>
-    		);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [
+        {
+          name: "naman",
+          percent: 60.0
+        },
+        {
+          name: "ritesh",
+          percent: 99.9431
+        },
+        {
+          name: "hitesh",
+          percent: 65.3365
+        },
+        {
+          name: "shreya",
+          percent: 80.64
+        },
+        {
+          name: "ankur",
+          percent: 78.897
+        },
+        {
+          name: "vanshu",
+          percent: 78.9876
+        },
+        {
+          name: "krupal",
+          percent: 85.3214
+        }
+      ]
+    };
+  }
+  render() {
+    return (
+      <div id="main">
+        {
+          //Correct the percentage conditional for correct output
+          this.state.list.map(function (item, index) {
+            if (item.percent > 75) {
+              return (
+                //Conditional statement in the className of below div to give callName="bg-pink"
+                //if the percent >= 90 else className = ""
+                <div key={index} className={item.percent>=90?"bg-pink" : "name"}>
+                  <div className="name">
+                    <h1>{item.name}</h1>
+                  </div>
+                  <div className="percent">
+                    <h1>{item.percent.toFixed(2)}</h1>
+                  </div>
+                </div>
+              );
+            }
+            
+          })
+        }
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
-
